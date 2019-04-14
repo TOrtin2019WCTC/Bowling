cf7228ac13389cc77babf4aa0c077ef798a9646a
tortin2019wctc <tortin@my.wctc.edu>
2019-04-14 15:04:32 -0500

    Added Test Suite class

---
+++ src/BowlingTestSuite.java
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Class runs Test Suite
+ *
+ * @author Tim Ortin, Tyler Kelly
+ *
+ * @version 1
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses (  {
+        FrameTest.class
+}  )
+public class BowlingTestSuite {
+}
